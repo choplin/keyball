54afad1ae653a7095b8b0fc3cd7e04b8f2b15af4
Akihiro Okuno <choplin.choplin@gmail.com>
2023-01-14 13:05:00 +0900

    Update keymap

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my/keymap.c
@@ -41,13 +41,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(ARROW, KC_ESC) , KC_Q    , KC_W    , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P           , LT(ARROW, KC_LBRC),
     LCTL_T(KC_TAB)    , KC_A    , KC_S    , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , RCTL_T(KC_SCLN), C_S_T(KC_RBRC) ,
     LSFT_T(KC_MINS)   , KC_Z    , KC_X    , KC_C     , KC_V     , KC_B     ,                                  KC_N     , KC_M     , KC_COMM  , KC_DOT   , RSFT_T(KC_SLSH), RALT_T(KC_BSLS),
-                                  TO(BASE), KC_F12,     KC_BSPC ,KC_SPC, KC_LCTL,                   KC_RCTL, LT(NUM,KC_ENT)       , _______  , _______  , _______
+                                  TO(BASE), KC_F12,     KC_BSPC ,KC_SPC, KC_LGUI,                   KC_RGUI, LT(NUM,KC_ENT)       , _______  , _______  , _______
   ),
 
   [NUM] = LAYOUT_universal(
-    KC_EQL            ,  S(KC_1)   , S(KC_2)   , S(KC_3)   , S(KC_4)   , S(KC_5)   ,                                       S(KC_6)   , S(KC_7)   , S(KC_8)   , S(KC_9)   , S(KC_0) , KC_QUOT ,
-    LCTL_T(S(KC_EQL)) ,  KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,                                       KC_6      , KC_7      , KC_8      , KC_9      , KC_0    , S(KC_QUOT) ,
-    LSFT_T(KC_GRAVE)  ,  RCS(KC_1) , RCS(KC_2) , RCS(KC_3) , RCS(KC_4) , RCS(KC_5) ,                                       RCS(KC_6) , RCS(KC_7) , RCS(KC_8) , RCS(KC_9) , KC_DEL  , S(KC_GRAVE) ,
+    KC_EQL      ,  S(KC_1)   , S(KC_2)   , S(KC_3)   , S(KC_4)   , S(KC_5)   ,                                       S(KC_6)   , S(KC_7)   , S(KC_8)   , S(KC_9)   , S(KC_0) , KC_QUOT ,
+    S(KC_EQL)   ,  KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,                                       KC_6      , KC_7      , KC_8      , KC_9      , KC_0    , S(KC_QUOT) ,
+    S(KC_GRAVE) ,  RCS(KC_1) , RCS(KC_2) , RCS(KC_3) , RCS(KC_4) , RCS(KC_5) ,                                       RCS(KC_6) , RCS(KC_7) , RCS(KC_8) , RCS(KC_9) , KC_DEL  , KC_GRAVE ,
                                      _______   , _______       , _______ , _______ , _______ ,                      _______  , _______           , _______   , _______   , _______
   ),
 
