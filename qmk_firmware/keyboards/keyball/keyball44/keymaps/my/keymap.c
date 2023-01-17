13ae9c6b43b643479d0f83c681b56f818c107ba7
Akihiro Okuno <choplin.choplin@gmail.com>
2023-01-17 13:24:07 +0900

    Add system keymap layer

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my/keymap.c
@@ -33,8 +33,8 @@ enum layers {
     BASE = 0,
     NUM,
     MOUSE,
-    ARROW,
     SYSTEM,
+    ARROW,
 };
 
 // clang-format off
@@ -44,14 +44,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(ARROW, KC_ESC) , KC_Q    , KC_W    , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P           , LT(ARROW, KC_LBRC),
     LCTL_T(KC_TAB)    , KC_A    , KC_S    , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , RCTL_T(KC_SCLN), C_S_T(KC_RBRC) ,
     LSFT_T(KC_MINS)   , KC_Z    , KC_X    , KC_C     , KC_V     , KC_B     ,                                  KC_N     , KC_M     , KC_COMM  , KC_DOT   , RSFT_T(KC_SLSH), RALT_T(KC_BSLS),
-                                  TO(BASE), KC_F12,     KC_BSPC ,KC_SPC, KC_LGUI,                   KC_RGUI, LT(NUM,KC_ENT)       , _______  , _______  , KC_MEH
+                                  MO(SYSTEM), KC_F12, LSFT_T(KC_BSPC) ,KC_SPC, KC_LGUI,                   KC_RGUI, LT(NUM,KC_ENT)       , _______  , _______  , KC_MEH
   ),
 
   [NUM] = LAYOUT_universal(
     KC_EQL      ,  S(KC_1)   , S(KC_2)   , S(KC_3)   , S(KC_4)   , S(KC_5)   ,                                       S(KC_6)   , S(KC_7)   , S(KC_8)   , S(KC_9)   , S(KC_0) , KC_QUOT ,
     S(KC_EQL)   ,  KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,                                       KC_6      , KC_7      , KC_8      , KC_9      , KC_0    , S(KC_QUOT) ,
     S(KC_GRAVE) ,  RCS(KC_1) , RCS(KC_2) , RCS(KC_3) , RCS(KC_4) , RCS(KC_5) ,                                       RCS(KC_6) , RCS(KC_7) , RCS(KC_8) , RCS(KC_9) , KC_DEL  , KC_GRAVE ,
-                               _______   , _______       , KC_DEL , _______ , _______ ,              _______  , _______        , _______   , _______   , _______
+                               _______   , _______       , _______ , _______ , _______ ,              _______  , _______        , _______   , _______   , _______
   ),
 
   [MOUSE] = LAYOUT_universal(
@@ -61,20 +61,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         _______ , _______  ,    _______  , _______  , _______  ,                _______  , _______        , _______ , _______ , _______
   ),
 
-  [ARROW] = LAYOUT_universal(
-    _______ , _______  , KBC_RST , KBC_SAVE , SCRL_DVD , SCRL_DVI ,                                       KC_HOME    , KC_PGDN    , KC_PGUP  , KC_END      , _______ , _______ ,
-    _______ , RGB_HUI  , RGB_SAI , RGB_VAI  , CPI_D1K  , CPI_I1K  ,                                       KC_LEFT    , KC_DOWN    , KC_UP    , KC_RIGHT    , _______ , _______ ,
-    _______ , RGB_HUD  , RGB_SAD , RGB_VAD  , CPI_D100 , CPI_I100 ,                                       C(KC_LEFT) , C(KC_DOWN) , C(KC_UP) , C(KC_RIGHT) , _______ , _______ ,
-                       _______ , _______              , _______ , _______ , QK_BOOT ,                      _______  , _______              , _______  , _______     , _______
-  ),
-  
   [SYSTEM] = LAYOUT_universal(
-    RGB_TOG          , _______  , KBC_RST , KBC_SAVE , SCRL_DVD , SCRL_DVI ,                                       KC_HOME    , KC_PGDN    , KC_PGUP  , KC_END      , _______ , _______ ,
-    LCTL_T(RGB_MOD)  , RGB_HUI  , RGB_SAI , RGB_VAI  , CPI_D1K  , CPI_I1K  ,                                       KC_LEFT    , KC_DOWN    , KC_UP    , KC_RIGHT    , _______ , _______ ,
-    LSFT_T(RGB_RMOD) , RGB_HUD  , RGB_SAD , RGB_VAD  , CPI_D100 , CPI_I100 ,                                       C(KC_LEFT) , C(KC_DOWN) , C(KC_UP) , C(KC_RIGHT) , _______ , _______ ,
-                       _______ , _______              , _______ , _______ , QK_BOOT ,                      _______  , _______              , _______  , _______     , _______
+    RGB_TOG  , _______  , _______ , _______ , _______ , _______ ,                                       KBC_SAVE , _______ , _______  , _______ , _______ , _______ ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI , RGB_VAI , _______ , _______ ,                                       SCRL_DVI , CPI_I1K , CPI_I100 , _______ , _______ , _______ ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD , RGB_VAD , _______ , _______ ,                                       SCRL_DVD , CPI_D1K , CPI_D100 , _______ , _______ , _______ ,
+                       _______ , _______              , EE_CLR , _______ , QK_BOOT ,       _______  , _______              , _______  , _______ , _______
   ),
-  
+
+  [ARROW] = LAYOUT_universal(
+    _______ , _______  , _______ , _______ , _______ , _______ ,                                       KC_HOME    , KC_PGDN    , KC_PGUP  , KC_END      , _______ , _______ ,
+    _______ , _______  , _______ , _______ , _______ , _______ ,                                       KC_LEFT    , KC_DOWN    , KC_UP    , KC_RIGHT    , _______ , _______ ,
+    _______ , _______  , _______ , _______ , _______ , _______ ,                                       C(KC_LEFT) , C(KC_DOWN) , C(KC_UP) , C(KC_RIGHT) , _______ , _______ ,
+                         _______ , _______              , _______ , _______ , _______ ,       _______  , _______               , _______  , _______     , _______
+  ),
 };
 // clang-format on
 
@@ -152,6 +151,9 @@ void my_oled_render_info(void) {
         break;
     case ARROW:
         oled_write_P(PSTR("Arrow"), false);
+        break;
+    case SYSTEM:
+        oled_write_P(PSTR("Sys  "), false);
         break;
     default:
         break;
